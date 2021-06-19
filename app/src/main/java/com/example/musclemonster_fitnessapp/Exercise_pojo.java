503400062a8269ee2c5dcec55de73ccdb2f21131
rohan <rohanjariwala03@gmail.com>
2021-06-19 13:21:17 -0400

    Trying to get data from firebase

--- app/src/main/java/com/example/musclemonster_fitnessapp/Exercise_pojo.java
+++ app/src/main/java/com/example/musclemonster_fitnessapp/Exercise_pojo.java
@@ -1,13 +1,10 @@
 package com.example.musclemonster_fitnessapp;
 
 public class Exercise_pojo {
-    public String Exercise;
+    private String Exercise;
 
     public Exercise_pojo(){
 
-    }
-    public Exercise_pojo(String Exercise){
-        this.Exercise=Exercise;
     }
 
     public String getExercise(){
