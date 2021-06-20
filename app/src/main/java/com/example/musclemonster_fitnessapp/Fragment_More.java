048501f46c66eaf90bd5c2f7e287293cbc82ddd8
rohan <rohanjariwala03@gmail.com>
2021-06-20 01:38:25 -0400

    More Tab Color.xml file for button Resources file for button

--- app/src/main/java/com/example/musclemonster_fitnessapp/Fragment_More.java
+++ app/src/main/java/com/example/musclemonster_fitnessapp/Fragment_More.java
@@ -27,6 +27,7 @@ public class Fragment_More extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment__more, container, false);
+        View view=inflater.inflate(R.layout.fragment__more, container, false);
+        return view;
     }
 }
