bf6ebcbfda79c2ebb2b623e4bba5debc3fce9a41
rohan <rohanjariwala03@gmail.com>
2021-06-21 20:28:27 -0400

    Completion of add_Product_HOme "MIHIR"

--- app/src/main/java/com/example/musclemonster_fitnessapp/Product_Add_Home.java
+++ app/src/main/java/com/example/musclemonster_fitnessapp/Product_Add_Home.java
@@ -5,6 +5,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.ContentResolver;
 import android.content.Intent;
@@ -12,12 +13,14 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.Layout;
 import android.util.Log;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,20 +34,25 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.IOException;
+import java.security.PrivateKey;
 
 public class Product_Add_Home extends AppCompatActivity {
 
-    public static int id = 1;
-    String Storage_Path;
-    String Database_Path;
-    Button BtnShow,BtnUpload,BtnSubmit;
-    EditText ProName,ProWeight,ProCat,ProPrice,ProDesc;
-    Uri FilePathUri;
-    ImageView ImgView;
-    StorageReference storageReference;
-    DatabaseReference databaseReference;
+    private static int id = 1;
+    private String Storage_Path;
+    private String Database_Path;
+    private Button BtnUpload,BtnSubmit;
+    private EditText ProName,ProWeight,ProCat,ProPrice,ProDesc;
 
-    ProgressDialog progressDialog ;
+    private ImageView ImgView;
+    private StorageReference storageReference;
+    private DatabaseReference databaseReference;
+
+    private Uri resultUri;
+
+    private ProgressDialog progressDialog ;
+
+    private LinearLayout ImageLL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,35 +78,32 @@ public class Product_Add_Home extends AppCompatActivity {
         ProPrice = (EditText) findViewById(R.id.EditProdPrice);
         ProDesc = (EditText) findViewById(R.id.EditProdDesc);
 
-        BtnShow = (Button) findViewById(R.id.BtnShowImage);
         BtnUpload = (Button) findViewById(R.id.BtnUploadImage);
         BtnSubmit = (Button) findViewById(R.id.BtnSubmit);
 
-        //ImgView = (ImageView) findViewById(R.id.ShowImageView);
+        ImgView = (ImageView) findViewById(R.id.ShowImageView);
 
         progressDialog = new ProgressDialog(Product_Add_Home.this);
 
-        /*BtnShow.setOnClickListener(new View.OnClickListener() {
+        ImageLL = (LinearLayout) findViewById(R.id.ImgLinearLayout);
+
+        ImageLL.setVisibility(View.INVISIBLE) ;
+
+        BtnUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ImageLL.setVisibility(View.VISIBLE) ;
                 // Creating intent.
                 Intent intent = new Intent();
 
                 // Setting intent type as image to select image from phone storage.
                 intent.setType("image/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent,1);
                 //startActivityForResult(Intent.createChooser(intent, "Please Select Image"),1);
+
             }
         });
-
-
-        BtnUpload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Calling method to upload selected image on Firebase storage.
-
-            }
-        });*/
 
 
         BtnSubmit.setOnClickListener(new View.OnClickListener() {
@@ -112,7 +117,7 @@ public class Product_Add_Home extends AppCompatActivity {
 
     private void UploadData() {
 
-        String ProductName = ProName.getText().toString().trim();
+       /* String ProductName = ProName.getText().toString().trim();
         String ProductWeight = ProWeight.getText().toString().trim();
         String ProductPrice = ProPrice.getText().toString().trim();
         String ProductCat = ProCat.getText().toString().trim();
@@ -126,7 +131,122 @@ public class Product_Add_Home extends AppCompatActivity {
         databaseReference.push().setValue(ProUploadPOJO);
         Log.i("Product_Add_Home", "Datax : " + id);
         Log.i("Product_Add_Home", "Datax : " + ProUploadPOJO.getProductName());
-        Toast.makeText(Product_Add_Home.this,"Inserted",Toast.LENGTH_LONG).show();
+        Toast.makeText(Product_Add_Home.this,"Inserted",Toast.LENGTH_LONG).show();*/
+
+
+
+        // Checking whether FilePathUri Is empty or not.
+        if (resultUri != null) {
+
+            // Setting progressDialog Title.
+            progressDialog.setTitle("Image is Uploading...");
+
+            // Showing progressDialog.
+            progressDialog.show();
+
+            // Creating second StorageReference.
+            StorageReference storageReference2nd = storageReference.child(Storage_Path + System.currentTimeMillis() + "." + GetFileExtension(resultUri));
+
+            // Adding addOnSuccessListener to second StorageReference.
+            storageReference2nd.putFile(resultUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                            // Getting image name from EditText and store into string variable.
+                            String ProductName = ProName.getText().toString().trim();
+                            String ProductWeight = ProWeight.getText().toString().trim();
+                            String ProductPrice = ProPrice.getText().toString().trim();
+                            String ProductCat = ProCat.getText().toString().trim();
+                            String ProductDesc = ProDesc.getText().toString().trim();
+                            String ImgUri = "Default";
+                            // Hiding the progressDialog after done uploading.
+                            progressDialog.dismiss();
+
+                            // Showing toast message after done uploading.
+                            Toast.makeText(getApplicationContext(), "Image Uploaded Successfully ", Toast.LENGTH_LONG).show();
+
+                            @SuppressWarnings("VisibleForTests")
+                            ProductUpload_POJO ProUploadPOJO = new ProductUpload_POJO(ProductName,ProductWeight,ProductPrice,ProductCat,ProductDesc,taskSnapshot.getMetadata().getReference().getDownloadUrl().toString());
+                            //ImageUploadInfo imageUploadInfo = new ImageUploadInfo(TempImageName, taskSnapshot.getDownloadUrl().toString());
+
+                            // Getting image upload ID.
+                            String ImageUploadId = databaseReference.push().getKey();
+
+                            // Adding image upload id s child element into databaseReference.
+                            //databaseReference.child(ImageUploadId).setValue(ProUploadPOJO);
+                            databaseReference.push().setValue(ProUploadPOJO);
+                        }
+                    })
+                    // If something goes wrong .
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+
+                            // Hiding the progressDialog.
+                            progressDialog.dismiss();
+
+                            // Showing exception erro message.
+                            Toast.makeText(Product_Add_Home.this, exception.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    })
+
+                    // On progress change upload time.
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+
+                            // Setting progressDialog Title.
+                            progressDialog.setTitle("Image is Uploading...");
+
+                        }
+                    });
+        }
+        else {
+
+            Toast.makeText(Product_Add_Home.this, "Please Select Image or Add Image Name", Toast.LENGTH_LONG).show();
+
+        }
     }
 
+    // Creating Method to get the selected image file Extension from File Path URI.
+    public String GetFileExtension(Uri uri) {
+
+        ContentResolver contentResolver = getContentResolver();
+
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+
+        // Returning the file Extension.
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri)) ;
+
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == Activity.RESULT_OK){
+            final Uri imageUri = data.getData();
+            resultUri = imageUri;
+            //ImgView.setImageURI(resultUri);
+
+            try {
+
+                // Getting selected image into Bitmap.
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), resultUri);
+
+                // Setting up bitmap selected image into ImageView.
+                ImgView.setImageBitmap(bitmap);
+
+                // After selecting image change choose button above text.
+                //BtnUpload.setText("Image Selected");
+
+            }
+            catch (IOException e) {
+
+                e.printStackTrace();
+            }
+        }
+    }
 }
