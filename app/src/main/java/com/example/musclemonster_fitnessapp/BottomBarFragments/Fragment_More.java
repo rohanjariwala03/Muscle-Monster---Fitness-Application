c17173ad31c6350e29e4f0268f3f77e4f7eafff4
rohan <rohanjariwala03@gmail.com>
2021-06-24 03:16:43 -0400

    Admin side Menu Bar

--- app/src/main/java/com/example/musclemonster_fitnessapp/BottomBarFragments/Fragment_More.java
+++ app/src/main/java/com/example/musclemonster_fitnessapp/BottomBarFragments/Fragment_More.java
@@ -11,11 +11,13 @@ import androidx.fragment.app.Fragment;
 
 import com.example.musclemonster_fitnessapp.MoreMenuClasses.Product_Add_Home;
 import com.example.musclemonster_fitnessapp.R;
+import com.example.musclemonster_fitnessapp.Update_Profile;
 
 
 public class Fragment_More extends Fragment {
 
     Button btn_addItem;
+    Button btn_MyProfile;
 
     public Fragment_More() {
         // Required empty public constructor
@@ -35,7 +37,15 @@ public class Fragment_More extends Fragment {
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragment__more, container, false);
         btn_addItem =view.findViewById(R.id.btnSellProducts);
+        btn_MyProfile =view.findViewById(R.id.btnMyProfile);
 
+        btn_MyProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), Update_Profile.class);
+                startActivity(intent);
+            }
+        });
         btn_addItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
